3942ac6297bfaa6178d09716d77b06ee5b7e298c
Nicole Alday <nicole.aday1111@gmail.com>
2025-02-17 18:47:52 +0800

    chore: initial project setup with vue

---
+++ src/views/BookDoctorView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h3>Welcome to ShiftCare</h3>
+    <p>
+      Find your healthcare professional now and get the expert care you deserve, right at your
+      fingertips.
+    </p>
+    <button>Start</button>
+  </main>
+</template>
